bb5701d551e4c64297b89e63bd431f5d5576eb1f
mbugaisen <mkbugaisen@cogeco.ca>
2020-03-08 20:31:24 -0400

    Fixed an issue with the pmod tvalid signal not being driven to start the transfer.

--- packet_switcher_arbiter_code/port_controller.sv
+++ packet_switcher_arbiter_code/port_controller.sv
@@ -6,14 +6,14 @@ module port_controller (
 	input buf_axis_tvalid,
 	input [7:0] buf_axis_tdata,
 	input buf_axis_tlast,
-	output reg buf_axis_tready,
+	output wire buf_axis_tready,
 
 	// AXI Stream signals from meta data buffer
 	input meta_axis_tvalid,
 	input [31:0] meta_axis_tdata,
 	output reg meta_axis_tready,
 	
-	// AXi Stream to PMOD
+	// AXI Stream to PMOD
     input s_axis_tready,
     output reg [31:0] s_axis_tdata,
     output reg s_axis_tlast,
@@ -31,12 +31,13 @@ module port_controller (
 		WRITING
 	} pmod_port_state;
 
+	assign buf_axis_tready = s_axis_tready;
+
 	// State machine for PMOD port
 	always @(posedge clk) begin
 		if(~resetn) begin
 			pmod_port_state <= IDLE;
 
-			buf_axis_tready <= 1'b0;
 			s_axis_tdata <= 32'd0;
 			s_axis_tlast <= 1'b0;
 			s_axis_tvalid <= 1'b0;
@@ -50,9 +51,13 @@ module port_controller (
 				IDLE: begin
 					meta_axis_tready <= 1'b0;
 					s_axis_tvalid <= 1'b0;
-					buf_axis_tready <= 1'b0;
 				
 					if(start_transfer) begin
+						// Let the PMOD know that we want to write to it
+						s_axis_tvalid <= 1'b1;
+						s_axis_tdata[7:0] <= buf_axis_tdata;
+						s_axis_tlast <= buf_axis_tlast;
+
 						pmod_port_state <= WRITING;
 						ready_transfer <= 1'b0;
 					end
@@ -60,8 +65,7 @@ module port_controller (
 				WRITING: begin
 					// PMOD ready for more data
 					if(s_axis_tready) begin
-						s_axis_tvalid <= 1'b1;
-						buf_axis_tready <= 1'b1;
+						//s_axis_tvalid <= 1'b1;
 						s_axis_tdata[7:0] <= buf_axis_tdata;
 						s_axis_tlast <= buf_axis_tlast;
 
@@ -73,9 +77,6 @@ module port_controller (
 							
 							ready_transfer <= 1'b1;
 						end
-					end
-					else begin
-						buf_axis_tready <= 1'b0;
 					end
 				end
 			endcase
